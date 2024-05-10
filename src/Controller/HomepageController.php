4cf103aaf90955b753b4d839c494d2b7c0e1ce49
aleattene <attene.tomorrowdevs@gmail.com>
2024-05-10 17:16:56 +0200

    feat(homepage): add initial json response

---
+++ src/Controller/HomepageController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class HomepageController extends AbstractController
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    #[Route('/', name: 'homepage')]
+    public function index(): Response
+    {
+        return new JsonResponse(([
+            "message" => "Hello, world!",
+            "timestamp" => date('Y-m-d H:i:s', time())
+        ]));
+    }
+}
