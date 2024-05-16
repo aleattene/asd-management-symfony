2d9ff7c4e530040030abe898e8299782cd58fda1
aleattene <attene.tomorrowdevs@gmail.com>
2024-05-16 10:16:01 +0200

    feat(admin-dashboard): add Trainer and Athletes crud controllers

---
+++ src/Controller/Admin/AthleteCrudController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Athlete;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class AthleteCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Athlete::class;
+    }
+
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id')->hideOnForm(),
+            TextField::new('name'),
+            TextField::new('surname'),
+            AssociationField::new('trainer'),
+        ];
+    }
+
+}
